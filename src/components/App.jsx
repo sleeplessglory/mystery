e9ed512fdc823e1e6e608b142b28fe95e73a76b4
sleeplessglory <91571026+sleeplessglory@users.noreply.github.com>
2024-10-08 23:32:13 +0300

    Layout sketch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,31 @@
+
 export default function App() {
-    
+    return(
+        <>
+            <div className="grid">
+                <div className="grid__name">
+
+                </div>
+                <div className="grid__leaders">
+
+                </div>
+                <div className="grid__info">
+                    <div className="info__blocks">Blocks</div>
+                    <div className="info__coins">Coins</div>
+                    <div className="info__invites">Invites</div>
+                </div>
+                <div className="grid__time">
+                    <div className="time__hour">Hour</div>
+                    <div className="time__day">Day</div>
+                    <div className="time__week">Week</div>
+                </div>
+                <div className="grid__results">
+
+                </div>
+                <div className="grid__user-result">
+
+                </div>
+            </div>
+        </>
+    );
 }
